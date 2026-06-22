20809c75d1c03e8a8aeaaa76caddd6e3d7843a61
Kenzo Sebastian <kenzosebastian4@gmail.com>
2026-06-22 13:56:11 +0700

    fix: deploy vercel

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TescoModule } from './tesco/tesco.module';
 
 @Module({
-  imports: [TescoModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
