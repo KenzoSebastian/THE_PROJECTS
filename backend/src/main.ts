61e4f8b07dc1e70fc75845ddb6fc2bd998573e5f
Kenzo Sebastian <kenzosebastian4@gmail.com>
2026-06-23 22:58:56 +0700

    fix(prod): roll back 2  to bulletproof relative paths for prisma service

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,7 @@
-import { register } from 'tsconfig-paths';
-import * as tsConfig from '../tsconfig.json';
-
-register({
-  baseUrl: tsConfig.compilerOptions.baseUrl || './',
-  paths: (tsConfig.compilerOptions as any).paths || {},
-});
-
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UniversalExceptionFilter } from './common/filters/universal-exception.filter';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
